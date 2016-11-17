8321d455f269b50948553fbbb6b74e8817e3b4d9
Vipin Chaudhary <vipin14119@iiitd.ac.in>
2016-11-17 22:26:49 +0530

    Some changes in links

--- main/urls.py
+++ main/urls.py
@@ -3,5 +3,6 @@ import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^login/$', views.login, name='login'),
 
 ]
--- main/views.py
+++ main/views.py
@@ -4,3 +4,9 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("OK")
+
+def login(request):
+    if request.method == "POST":
+        return HttpResponse("GOT POST REQUEST IN LOGIN ")
+    else:
+        return HttpResponse("GOT GET REQUEST IN LOGIN ")
