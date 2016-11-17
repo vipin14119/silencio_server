d5c5c1aaf393dbb0dd1626e2fc4c6430201e160e
Vipin Chaudhary <vipin14119@iiitd.ac.in>
2016-11-17 09:55:01 +0530

    added setting.local for database

---
+++ silencio_server/silencio_server/setting_local.py
@@ -0,0 +1,10 @@
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'silencio',
+        'USER':'root',
+        'PASSWORD':'root',
+        'port': '',
+        'HOST':'',
+    }
+}
