5f7938ef22be57d50e99dbfce88dc1606ba8a054
Vipin Chaudhary <vipin14119@iiitd.ac.in>
2016-11-17 10:12:50 +0530

    Changed root directory

---
+++ silencio_server/setting_local.py
@@ -0,0 +1,10 @@
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'silencio',
+        'USER':'root',
+        'PASSWORD':'09091996',
+        'port': '',
+        'HOST':'',
+    }
+}
