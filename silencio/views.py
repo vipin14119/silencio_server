b7bc92d3a984118a75af30818e6f92402045aa9f
Vipin Chaudhary <vipin14119@iiitd.ac.in>
2016-11-24 22:06:09 +0530

    testing json.dumps with androdi asyntask

--- silencio/views.py
+++ silencio/views.py
@@ -1,12 +1,24 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+import json
 
 def get_data(request):
-    return HttpResponse("OK GOT IT")
-    
-def login(request):
-    if request.method == "POST":
-        return HttpResponse("GOT POST REQUEST IN LOGIN ")
-    else:
-        return HttpResponse("GOT GET REQUEST IN LOGIN ")
+    obj1 = {
+    "name" : "Vipin Chaudhary",
+    "roll_no" : 2014119,
+    "college" : "IIIT Delhi",
+    "weight" : float(61.8)
+    }
+    obj2 = {
+    "name" : "Monu Chaudhary",
+    "roll_no" : 2014120,
+    "college" : "IIIT Delhi",
+    "weight" : float(64.8)
+    }
+    obj3 = {
+    "name" : "Shrishtee Gupta",
+    "roll_no" : 2014118,
+    "college" : "IIIT Delhi",
+    "weight" : float(49.8)
+    }
+    return HttpResponse(json.dumps([obj1, obj2, obj3]))
