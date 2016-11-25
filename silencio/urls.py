37b9579182f91be36de08ef89114dec756a47c09
Vipin Chaudhary <vipin14119@iiitd.ac.in>
2016-11-25 23:25:47 +0530

    Added new url for csrf token

--- silencio/urls.py
+++ silencio/urls.py
@@ -4,4 +4,5 @@ import views
 urlpatterns = [
     url(r'^$', views.get_data, name='get_data'),
     url(r'^post/$', views.post_data, name='post_data'),
+    url(r'^get_csrf/$', views.get_csrf_token, name='get_csrf'),
 ]
