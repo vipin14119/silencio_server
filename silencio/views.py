a0e8e24a945da4c9b467a8e7874d9bee57202e46
Vipin Chaudhary <vipin14119@iiitd.ac.in>
2016-11-28 17:36:07 +0530

    return values defined for every possible cases

--- silencio/views.py
+++ silencio/views.py
@@ -5,10 +5,22 @@ from django.views.decorators.csrf import csrf_exempt, ensure_csrf_cookie
 from silencio.models import Record, User
 import datetime
 
-OK_FLAG = "1"
-NOTOK_FLAG = "0"
-BAD_FLAG = "-1"
+# Login Return Values
+LOGIN_OK = 1
+USERNAME_DOESNT_EXIST = 2
+INCORRECT_PASSWORD = 3
+LOGIN_SERVER_ERROR = 4
 
+# Signup Return Values
+SIGNUP_OK = 5
+USERNAME_EXIST = 6
+SIGNUP_SERVER_ERROR = 7
+PARAMETERS_INCORRECT = 8
+
+# Post Record Bundle Return Values
+DATETIME_FORMAT_ERROR = 9
+SUCCESS_MSG = 'RECORD CREATED SUCCESSFULLY'
+WRONG_METHOD = 'ITS NOT THE CORRECT METHOD FOR THIS URL'
 
 def get_data(request):
     obj1 = {
@@ -35,25 +47,28 @@ def get_data(request):
 @csrf_exempt
 def post_record_bundle(request):
     if request.method == "POST":
-        username = request.POST.get("username")
-        user = User.objects.get(username=username)
-        place = request.POST.get("place")
-        start_time = request.POST.get("start_time")
-        end_time = request.POST.get("end_time")
-        db_level = float(request.POST.get("db_level"))
-        start_time = datetime.datetime.strptime(start_time, "%Y/%m/%d %H:%M:%S")
-        end_time = datetime.datetime.strptime(end_time, "%Y/%m/%d %H:%M:%S")
-        Record(user=user, name=place, db_level=db_level, start_time=start_time, end_time=end_time).save()
-        # data = json.loads(request.POST.get('data'))
-        # print data['username']
-        return HttpResponse(start_time)
-        # return render(request, 'silencio/index.html', {
-        # 'user': user,
-        # 'start': start_time,
-        # 'end': end_time
-        # })
+        try:
+            username = request.POST.get("username")
+            place = request.POST.get("place")
+            start_time = request.POST.get("start_time")
+            end_time = request.POST.get("end_time")
+            db_level = float(request.POST.get("db_level"))
+        except:
+            return HttpResponse(PARAMETERS_INCORRECT)
+        try:
+            user = User.objects.get(username=username)
+        except:
+            return HttpResponse(USERNAME_DOESNT_EXIST)
+        try:
+            start_time = datetime.datetime.strptime(start_time, "%Y/%m/%d %H:%M:%S")
+            end_time = datetime.datetime.strptime(end_time, "%Y/%m/%d %H:%M:%S")
+        except:
+            return HttpResponse(DATETIME_FORMAT_ERROR)
+        record = Record(user=user, name=place, db_level=db_level, start_time=start_time, end_time=end_time)
+        record.save()
+        return HttpResponse(SUCCESS_MSG)
     else:
-        return HttpResponse("didnt get post data")
+        return HttpResponse(WRONG_METHOD)
 
 
 @ensure_csrf_cookie
@@ -64,25 +79,38 @@ def get_csrf_token(request):
 @csrf_exempt
 def signup(request):
     if request.method == 'POST':
-        username = request.POST.get("username")
-        password = request.POST.get("password")
-        User(username=username, password=password).save()
-        return HttpResponse(OK_FLAG)
+        try:
+            username = request.POST.get("username")
+            password = request.POST.get("password")
+        except:
+            return HttpResponse(PARAMETERS_INCORRECT)
+
+        if User.objects.filter(username=username).exists():
+            return HttpResponse(USERNAME_EXIST)
+        else:
+            User(username=username, password=password).save()
+            return HttpResponse(SIGNUP_OK)
     else:
-        return HttpResponse(BAD_FLAG)
+        return HttpResponse(SIGNUP_SERVER_ERROR)
 
 
 @csrf_exempt
 def login(request):
     if request.method == 'POST':
-        username = request.POST.get("username")
-        password = request.POST.get("password")
-        if User.objects.filter(username=username, password=password).exists():
-            return HttpResponse(OK_FLAG)
+        try:
+            username = request.POST.get("username")
+            password = request.POST.get("password")
+        except:
+            return HttpResponse(PARAMETERS_INCORRECT)
+        obj = User.objects.filter(username=username)
+        if obj.exists() and obj[0].password == password:
+            return HttpResponse(LOGIN_OK)
+        elif obj.exists() and obj[0].password != password:
+            return HttpResponse(INCORRECT_PASSWORD)
         else:
-            return HttpResponse(BAD_FLAG)
+            return HttpResponse(USERNAME_DOESNT_EXIST)
     else:
-        return HttpResponse(NOTOK_FLAG)
+        return HttpResponse(LOGIN_SERVER_ERROR)
 
 
 # curl -H "Content-Type: application/x-www-form-urlencoded" -X POST --data data='{"username":"xyz","password":"xyz"}'
