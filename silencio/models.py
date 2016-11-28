6f4fb363a0fbc11a63b88b77580a498ae18e5699
Vipin Chaudhary <vipin14119@iiitd.ac.in>
2016-11-28 09:52:38 +0530

    user models created

--- silencio/models.py
+++ silencio/models.py
@@ -1,10 +1,17 @@
 from __future__ import unicode_literals
 from django.db import models
 
+
+class User(models.Model):
+    username = models.CharField(max_length=100)
+    password = models.CharField(max_length=25)
+
+
 class Record(models.Model):
+    user = models.ForeignKey(User)
     name = models.CharField(max_length=1000)
     created_at = models.DateTimeField(auto_now=True)
     db_level = models.FloatField()
 
     def __str__(self):
-        return self.name + " : "+ str(self.db_level)
+        return self.name + " : " + str(self.db_level)
