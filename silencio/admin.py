28290494236fdb38852e708d7f473912daca048d
Vipin Chaudhary <vipin14119@iiitd.ac.in>
2016-11-28 10:23:33 +0530

    registered in admin

--- silencio/admin.py
+++ silencio/admin.py
@@ -1,9 +1,14 @@
 from django.contrib import admin
-from silencio.models import Record
+from silencio.models import Record, User
+
+
+class UserClass(admin.ModelAdmin):
+    class Meta:
+        model = User
+admin.site.register(User, UserClass)
+
 
 class RecordClass(admin.ModelAdmin):
     class Meta:
         model = Record
-
 admin.site.register(Record, RecordClass)
-# Register your models here.
