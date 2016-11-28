7ed59ec02db10b37ba8515b1864298e8696d4e79
Vipin Chaudhary <vipin14119@iiitd.ac.in>
2016-11-28 10:38:28 +0530

    login and signup url created

--- silencio/urls.py
+++ silencio/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     url(r'^$', views.get_data, name='get_data'),
     url(r'^post/$', views.post_record, name='post_data'),
     url(r'^get_csrf/$', views.get_csrf_token, name='get_csrf'),
+    url(r'^login/$', views.login, name='login'),
+    url(r'^signup/$', views.signup, name='signup'),
 ]
