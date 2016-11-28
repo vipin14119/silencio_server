d340cfdd3e2027f8093c1a1d1e872e6f669b7aae
Vipin Chaudhary <vipin14119@iiitd.ac.in>
2016-11-28 10:36:25 +0530

    flag and post url changed

--- silencio/views.py
+++ silencio/views.py
@@ -4,37 +4,44 @@ import json
 from django.views.decorators.csrf import csrf_exempt, ensure_csrf_cookie
 from silencio.models import Record, User
 
+OK_FLAG = "1"
+NOTOK_FLAG = "0"
+BAD_FLAG = "-1"
+
+
 def get_data(request):
     obj1 = {
-    "name" : "Vipin Chaudhary",
-    "roll_no" : 2014119,
-    "college" : "IIIT Delhi",
-    "weight" : float(61.8)
+        "name": "Vipin Chaudhary",
+        "roll_no": 2014119,
+        "college": "IIIT Delhi",
+        "weight": float(61.8)
     }
     obj2 = {
-    "name" : "Sonia Kauhsik",
-    "roll_no" : 2014120,
-    "college" : "IIIT Delhi",
-    "weight" : float(48)
+        "name": "Sonia Kauhsik",
+        "roll_no": 2014120,
+        "college": "IIIT Delhi",
+        "weight": float(48)
     }
     obj3 = {
-    "name" : "Manisha Gupta",
-    "roll_no" : 2014118,
-    "college" : "IIIT Delhi",
-    "weight" : float(49.8)
+        "name": "Manisha Gupta",
+        "roll_no": 2014118,
+        "college": "IIIT Delhi",
+        "weight": float(49.8)
     }
     return HttpResponse(json.dumps([obj1, obj2, obj3]))
 
 
 @csrf_exempt
-def post_data(request):
+def post_record(request):
     if request.method == "POST":
-        names = str(request.POST.get("name"))
+        username = request.POST.get("username")
+        user = User.objects.get(username=username)
+        place = request.POST.get("name")
         db_level = float(request.POST.get("db_level"))
-        Record(name=names, db_level=db_level).save()
+        Record(user=user, name=place, db_level=db_level).save()
         # data = json.loads(request.POST.get('data'))
         # print data['username']
-        return render(request, 'silencio/index.html', {'request_data': request.POST})
+        return HttpResponse(OK_FLAG)
     else:
         return HttpResponse("didnt get post data")
 
@@ -49,9 +56,9 @@ def signup(request):
         username = request.POST.get("username")
         password = request.POST.get("password")
         User(username=username, password=password).save()
-        return HttpResponse("1")
+        return HttpResponse(OK_FLAG)
     else:
-        return HttpResponse("-1")
+        return HttpResponse(BAD_FLAG)
 
 
 def login(request):
@@ -59,11 +66,11 @@ def login(request):
         username = request.POST.get("username")
         password = request.POST.get("password")
         if User.objects.filter(username=username, password=password).exists():
-            return HttpResponse("1")
+            return HttpResponse(OK_FLAG)
         else:
-            return HttpResponse("-1")
+            return HttpResponse(BAD_FLAG)
     else:
-        return HttpResponse("0")
+        return HttpResponse(NOTOK_FLAG)
 
 
 # curl -H "Content-Type: application/x-www-form-urlencoded" -X POST --data data='{"username":"xyz","password":"xyz"}'
