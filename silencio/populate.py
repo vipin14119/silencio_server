b7ae188dbb5a6099bbb6983bf13b0b23ac3d1e9c
Vipin Chaudhary <vipin14119@iiitd.ac.in>
2016-11-29 06:12:08 +0530

    script done

--- silencio/populate.py
+++ silencio/populate.py
@@ -5,8 +5,10 @@ f = open(getcwd()+"/silencio/data.txt")
 datas = f.read().split("\n")
 for data in datas:
     if len(data) > 20:
-        print data.split(" == ")
-        Location(name=data[0], mac=data[1]).save()
+        a = data.split(" == ")
+        print a[0]
+        print a[1]
+        Location(name=a[0], mac=a[1]).save()
 
 
 print "SCRIPT DONE"
