b5494d5b2e29dd2c9bfc7b3f9211d44b0bfd2940
Vipin Chaudhary <vipin14119@iiitd.ac.in>
2016-12-01 22:29:25 +0530

    updated populate.py

--- silencio/populate.py
+++ silencio/populate.py
@@ -6,9 +6,9 @@ datas = f.read().split("\n")
 for data in datas:
     if len(data) > 20:
         a = data.split(" == ")
-        print a[0]
-        print a[1]
-        Location(name=a[0], mac=a[1]).save()
-
+        # print a[0]
+        # print a[1]
+        print('locationsMap.put("'+a[1]+'", "'+a[0]+'");')
+        # Location(name=a[0], mac=a[1]).save()
 
 print "SCRIPT DONE"
